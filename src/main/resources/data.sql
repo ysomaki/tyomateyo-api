8777d948d628443da634d357513c99c29e24c441
SOTAkkkk <>
2023-12-23 09:11:55 +0900

    🗃️ add password colum

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO "user" (id, name) VALUES (1, '織田 信長');
-INSERT INTO "user" (id, name) VALUES (2, '豊臣 秀吉');
-INSERT INTO "user" (id, name) VALUES (3, '徳川 家康');
+INSERT INTO "user" (id, name, password) VALUES (1, '織田 信長', '織田信長のパスワード');
+INSERT INTO "user" (id, name, password) VALUES (2, '豊臣 秀吉', '豊臣秀吉のパスワード');
+INSERT INTO "user" (id, name, password) VALUES (3, '徳川 家康', '徳川家康のパスワード');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS "user"
 (
     id   INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
